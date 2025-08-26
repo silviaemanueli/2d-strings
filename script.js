a5d5e05b7db6688a88bfd4861e92edc41e371be3
silviaemanueli <silvia.stadler@escola.pr.gov.br>
2025-08-26 14:58:53 +0000

    aula6

--- script.js
+++ script.js
@@ -5,7 +5,34 @@ function mostraPalavrasChave() {
 
   const campoResultado = document.querySelector("#resultado-palavrachave");
 
-  const palavras = texto.split(" ");
+  const palavrasChave = processaTexto(texto);
 
-  campoResultado.textContent = palavras.join(","_);
+  campoResultado.textContent = palavrasChave.join(", ");
+}
+
+ function processaTexto(texto){
+    let palavras = texto.split(/\P{L}+/u);
+    const frequencias= contaFrequencias(palavras);
+    let ordenadas= Object.keys(frequencias).sort(ordenaPalavras);
+    function ordenaPalavra(p1,p2){
+      return frequencias [p2]-frequencias [p1];
+    }
+    return ordenadas.slice(0,10);
+  }
+  function contaFrequencias(palavras) {
+    let frequencias
+  }
+    for (let i of palavras){
+      frequencias[i]=0;
+    
+     for (let j of palavras) {
+      if (palavras[i] == palavras[j]) {
+        frequencias[i]++;
+      }
+    }
+  }
+   // console.log(frequencias);
+    return palavras;
+    
+   
 }
